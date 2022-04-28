8fdafe21f8748f922743bf5d259989ed393e225b
Baaba <baabadampare@gmail.com>
2022-04-28 13:37:22 +0000

    edit experience section

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Header />
       <Companies />
       <Services />
-      {/*<Experience/>*/}
+      <Experience/>
     </div>
   );
 }
--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -3,18 +3,25 @@ import React from 'react'
 function Experience() {
   return (
     <div className="bg-[#211D32] container mx-auto px-28 pt-10">
-      <div clssName="grid lg:grid-cols-2 grid-cols-1">
-        <div className='flex'>
+      <div clssName="grid grid-cols-1 lg:grid-cols-2">
+        <div>
           <img
             src="https://zesty-cajeta-af510d.netlify.app/employee.svg"
             alt="Employee Experiencing Digital World"
           />
         </div>
-        <div className=''>
+        <div>
           <div className="w-10 h-1 from-[#7361F2] to-[#BD4FF1] bg-gradient-to-r"></div>
-          <h1 className='pt-4 font-semibold text-5xl'>Get an amazing experience with our top-quality team.</h1>
-          <p>Trusted by 5000+ companies worldwide. Trusted by 5000+ companies worldwide</p>
-          <button>Learn More</button>
+          <h1 className="pt-4 font-semibold text-5xl text-white">
+            Get an amazing experience with our top-quality team.
+          </h1>
+          <p className="text-[#696984]">
+            Trusted by 5000+ companies worldwide. Trusted by 5000+ companies
+            worldwide
+          </p>
+          <button className="rounded px-7 py-3 bg-[#6366F1] text-white">
+            Learn More
+          </button>
         </div>
       </div>
     </div>
