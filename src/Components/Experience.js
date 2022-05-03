6ca16aafb0d7b6cdb6aea959f8485e493d270bc8
Baaba <baabadampare@gmail.com>
2022-05-03 08:33:34 +0000

    adjust responsiveness in experience section

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Experience() {
   return (
-    <div className="bg-[#211D32] container mx-auto px-28 pt-14">
+    <div className="bg-[#211D32] container mx-auto lg:px-28 pt-14 px-10 pb-10`">
       <div className="grid grid-cols-1 lg:grid-cols-2">
         <div>
           <img
@@ -12,10 +12,10 @@ function Experience() {
         </div>
         <div>
           <div className="w-10 h-1 from-[#7361F2] to-[#BD4FF1] bg-gradient-to-r"></div>
-          <h1 className="pt-4 font-semibold text-5xl text-white">
+          <h1 className="pt-4 font-semibold lg:text-5xl text-3xl text-white">
             Get an amazing experience with our top-quality team.
           </h1>
-          <p className="text-[#696984] pt-5 pb-5 w-[400px]">
+          <p className="text-[#696984] pt-5 pb-5 lg:w-[400px]">
             Trusted by 5000+ companies worldwide. Trusted by 5000+ companies
             worldwide
           </p>
