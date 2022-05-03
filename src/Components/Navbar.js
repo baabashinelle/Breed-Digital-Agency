f2477e1d0e184e780f058dbbed9820e49209194e
Baaba <baabadampare@gmail.com>
2022-05-03 14:35:59 +0000

    edit navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -19,7 +19,7 @@ export default function Navbar() {
       {({ open }) => (
         <>
           <div className="max-w-7xl">
-            <div className="relative flex flex-row-reverse items-center justify-between h-16">
+            <div className="relative flex flex-row-reverse lg:flex-row items-center justify-between h-16">
               <div className="inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
