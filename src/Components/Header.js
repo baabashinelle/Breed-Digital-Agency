3da53a836472f49a248baeb92162e9c349c50ee7
Baaba <baabadampare@gmail.com>
2022-05-03 08:25:23 +0000

    adjust responsiveness in header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,11 +3,11 @@ import Navbar from './Navbar'
 
 function Header() {
   return (
-    <div className="from-[#2B263F] to-[#231E3D] bg-gradient-to-b w-full container mx-auto px-28 h-screen overflow-hidden">
+    <div className="from-[#2B263F] to-[#231E3D] bg-gradient-to-b w-full container mx-auto px-10 lg:px-28 h-screen overflow-hidden">
       <Navbar />
-      <div className="flex pt-8">
+      <div className="lg:flex lg:pt-8 ">
         <div className="w-[800px] container mx-auto pt-24">
-          <h1 className="font-bold lg:text-[4.2rem] w-[79%] text-white leading-[5rem]">
+          <h1 className="font-bold lg:text-[4.2rem] text-3xl lg:w-[79%] text-white lg:leading-[5rem]">
             The power of now, the{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-br from-[#6664F1] to-[#C94AF0]">
               {" "}
@@ -15,13 +15,13 @@ function Header() {
             </span>
             of you
           </h1>
-          <div className="flex justify-start mt-7">
+          <div className="lg:flex justify-start mt-7">
             <div>
               <button className="rounded px-7 py-3 bg-[#6366F1] text-white">
                 Work with us
               </button>
             </div>
-            <p className="w-[50%] text-[14px] text-gray-400 leading-6 pl-5">
+            <p className="w-[50%] text-[14px] text-gray-400 lg:leading-6 lg:pl-5 pt-4">
               Breed is a digital studio that offers several services such as
               UI/UX design to developers, we will provide the best service for
               those of you who use our services.
