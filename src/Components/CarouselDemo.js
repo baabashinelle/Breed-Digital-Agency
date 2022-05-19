adc90a58bb3035690006b3c79d377f58ac0b56cf
Baaba <baabadampare@gmail.com>
2022-05-19 13:11:19 +0000

    add alt prop to carousel images

--- src/Components/CarouselDemo.js
+++ src/Components/CarouselDemo.js
@@ -7,21 +7,48 @@ import { Carousel } from "react-responsive-carousel";
 function CarouselDemo() {
   return (
     <div className="w-full pt-10 bg-[#231E3D]">
-      <Carousel className='carousel'>
+      <Carousel className="carousel">
         <div className="grid lg:grid-cols-3 gap-4">
-          <img src="https://zesty-cajeta-af510d.netlify.app/2.svg" />
-          <img src="https://zesty-cajeta-af510d.netlify.app/1.svg" />
-          <img src="https://zesty-cajeta-af510d.netlify.app/3.svg" />
+          <img
+            src="https://zesty-cajeta-af510d.netlify.app/2.svg"
+            alt="Image"
+          />
+          <img
+            src="https://zesty-cajeta-af510d.netlify.app/1.svg"
+            alt="Image"
+          />
+          <img
+            src="https://zesty-cajeta-af510d.netlify.app/3.svg"
+            alt="Image"
+          />
         </div>
         <div className="grid lg:grid-cols-3 gap-4">
-          <img src="https://zesty-cajeta-af510d.netlify.app/3.svg" />
-          <img src="https://zesty-cajeta-af510d.netlify.app/2.svg" />
-          <img src="https://zesty-cajeta-af510d.netlify.app/1.svg" />
+          <img
+            src="https://zesty-cajeta-af510d.netlify.app/3.svg"
+            alt="Image"
+          />
+          <img
+            src="https://zesty-cajeta-af510d.netlify.app/2.svg"
+            alt="Image"
+          />
+          <img
+            src="https://zesty-cajeta-af510d.netlify.app/1.svg"
+            alt="Image"
+          />
         </div>
         <div className="grid lg:grid-cols-3 gap-4">
-          <img src="https://zesty-cajeta-af510d.netlify.app/1.svg" />
-          <img src="https://zesty-cajeta-af510d.netlify.app/2.svg" />
-          <img src="https://zesty-cajeta-af510d.netlify.app/3.svg" />
+          <img
+            src="https://zesty-cajeta-af510d.netlify.app/1.svg"
+            alt="Image"
+          />
+          <img
+            src="https://zesty-cajeta-af510d.netlify.app/2.svg"
+            alt="Image"
+          />
+          <img
+            src="https://zesty-cajeta-af510d.netlify.app/3.svg"
+            alt="Image"
+          />
         </div>
       </Carousel>
     </div>
